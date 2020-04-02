78390fc0bb2c3313b21de17c1a9b54f0c50b9bde
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-04-03 00:37:42 +0300

    init commit

---
+++ src/main/java/com/ivan/yuyuk/sqlassignment/SqlAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.yuyuk.sqlassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/yuyuk/sqlassignment/SqlAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.yuyuk.sqlassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
