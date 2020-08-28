b1577e1b368895400b7da8dbd470210f29e84d18
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-29 00:43:04 +0700

    ss

--- src/main/java/com/ivan/yuyuk/sqlassignment/entity/User.java
+++ src/main/java/com/ivan/yuyuk/sqlassignment/entity/User.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String password;
