1adb74eec437b7213d68c40c33f4af4b5a85f5e4
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-29 00:51:01 +0700

    bool change def

--- src/main/java/com/ivan/yuyuk/sqlassignment/entity/User.java
+++ src/main/java/com/ivan/yuyuk/sqlassignment/entity/User.java
@@ -12,6 +12,7 @@ public class User {
     private Long id;
     private String userName;
     private String password;
+    @Column(name = "active", columnDefinition = "INT(1)")
     private boolean active;
     private String roles;
 
