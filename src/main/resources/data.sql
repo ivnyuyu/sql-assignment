3f6ff9ce2a9e10332df311bec90527e4f6f179e6
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-29 00:23:45 +0700

    zz

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,5 +3,9 @@
 /*insert into assignment(id, description, correct_query)
 values (1, 'Получить полные сведения обо всех изделиях.', 'select * from j');*/
 
-insert into users(user_name, password, roles, active) values
-('IvanFerz','$2a$10$.2Oq92ikFDRL.145COPmZusK.T3kInP1aGnw1iJ39jcYB8EsFSOdy','USER',1);
+/*insert into users(user_name, password, roles, active) values
+('IvanFerz','$2a$10$.2Oq92ikFDRL.145COPmZusK.T3kInP1aGnw1iJ39jcYB8EsFSOdy','USER',1);*/
+
+insert into j (Jnum, Jnam, St, Ci) values
+(1,'Валенок','Active','Томск');
+
