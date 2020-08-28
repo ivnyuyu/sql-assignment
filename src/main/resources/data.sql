c24499b67d8c586777ab4931a316493270e43209
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-28 22:48:43 +0700

    finnaly fix, i hope

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,4 +4,4 @@ insert into assignment(id, description, correct_query)
 values (1, 'Получить полные сведения обо всех изделиях.', 'select * from j');
 
 insert into assignment(id, description, correct_query)
-values (2, 'Получить полные  сведения обо всех изделиях в Томске.', 'select * from j where St =\'Томск'');
+values (2, 'Получить полные  сведения обо всех изделиях в Томске.', 'select * from j where St =\'Томск\'');
