a746822c188c16a485886534846bc2019443b9ae
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-29 00:19:10 +0700

    ss

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@
 /*insert into assignment(id, description, correct_query)
 values (1, 'Получить полные сведения обо всех изделиях.', 'select * from j');*/
 
+insert into users(user_name, password, roles, active) values
+('IvanFerz','$2a$10$.2Oq92ikFDRL.145COPmZusK.T3kInP1aGnw1iJ39jcYB8EsFSOdy','USER',1);
