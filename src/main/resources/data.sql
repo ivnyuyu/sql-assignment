662dbb8761c2a666b4e702edd85e2271fae246da
Ivan Yuyukin <i.a.yuyukin@gmail.com>
2020-08-28 23:36:59 +0700

    fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 
-/*
-insert into assignment(id, description, correct_query)
-values (1, 'Получить полные сведения обо всех изделиях.', 'select * from j');
+
+/*insert into assignment(id, description, correct_query)
+values (1, 'Получить полные сведения обо всех изделиях.', 'select * from j');*/
 
 insert into assignment(id, description, correct_query)
-values (2, 'Получить полные  сведения обо всех изделиях в Томске.', 'select * from j where St =\'Томск\'');*/
+values (2, 'Получить полные  сведения обо всех изделиях в Томске.', 'select * from j where St =\'Томск\'');
 
-insert into assignment(id, description, correct_query)
-values (3, 'Получить номер детали, для которой нет другой детали, имеющей меньшее значение веса.', 'select Pnum from P where We = select min(We) from P');
+insert /*into assignment(id, description, correct_query)
+values (3, 'Получить номер детали, для которой нет другой детали, имеющей меньшее значение веса.', 'select Pnum from P where We = select min(We) from P');*/
